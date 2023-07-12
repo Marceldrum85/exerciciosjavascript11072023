6cbfa9351d8b9676700f29af1906d0d61dd481f1
Marcel de Lima Brito <marceldrum85@gmail.com>
2023-07-11 21:54:32 -0300

    javascript exe

---
+++ aula2/exercicios.js
@@ -0,0 +1,40 @@
+ let prompt = require("prompt-sync")()
+
+// let pNum = parseInt(prompt("Digite o primeiro numero: "))
+// let sNum = prompt("Digite o segundo numero: ")
+
+// let soma = pNum + parseInt (sNum) ; 
+
+
+// if(soma >10) {
+//     console.log("A soma é: " + soma +  "e é maior que 10" )
+
+// }else if (soma == 10 ){
+//     console.log("A soma é:" + soma + "e igual a 10")
+
+// }else{
+//     console.log("A soma é:" + soma + "e é menor que 10")
+// }
+
+// let letra = prompt("Digite uma letra: ")
+// let numero = prompt("Digite um numero:")
+
+// if(letra == "a" && numero == "1"){
+//     console.log("Acertou!!!")
+// }else{
+//     console.log("Errou!!!")
+// }
+
+
+// let numero = parseInt (prompt("Digite um numero: "))
+
+// let rest = numero % 2
+
+
+// if(rest != 0){
+//     console.log ("O numero é impar")
+
+// }else{
+//     console.log("O numero é par")
+// }
+
