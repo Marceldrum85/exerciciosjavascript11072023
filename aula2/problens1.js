04d22758e4268738122351eab36078e7a39821e2
Marcel de Lima Brito <marceldrum85@gmail.com>
2023-07-13 22:00:01 -0300

    exercicios problens1

--- aula2/problens1.js
+++ aula2/problens1.js
@@ -1,43 +1,4 @@
- let prompt = require("prompt-sync")()
-
-
-// let name = prompt("Digite seu nome: ")
-// let cargo = prompt("Digite seu cargo: ")
-
-// let salary = parseInt(prompt("Digite seu salário: "))
-
-// if(salary < 1000){
-//   salary = parseInt(salary*1.1)
-// }
-
-// console.log(`O funcionario ${name}, com a funcao ${cargo} tem um salario de ${salary}` )
-
-
-// let salary = parseInt(prompt("Digite seu salário: "))
-
-// if(salary < 500){
-//   salary = parseInt(salary*1.3)
-//   console.log(`novo salario ajustado e ${salary}`)
-// }else{
-//     console.log("nao tem direito de aumento")
-// }
-
-
-// let nota1 = parseInt(prompt("digite sua primeira nota: "))
-// let nota2 = parseInt(prompt("Digite sua segunda nota: "))
-// let nota3 = parseInt(prompt("Digite sua terceira nota: "))
-// let nota4 = parseInt(prompt("Digite sua quarta nota: "))
-
-// let media = ( (nota1 + nota2 + nota3 + nota4) / 4)
-
-// if(media >= 7){
-//     console.log(`aprovado sua media foi de ${media}`)
-// }else if( media < 7 && media >=5) {
-//     console.log(`recuperacao sua media foi de ${media}`)
-// }else{
-//     console.log("Reprovado")
-// }
-
+let prompt = require("prompt-sync")()
 
 
 // Escreva um programa que solicita ao usuário um nome de usuário e uma senha. Verifique se o
@@ -141,4 +102,3 @@ else if(valorCompra>100 && valorCompra<=200){
 else{
   console.log(`Voce tera um desconto de 20%, sua compra ficara no valor de ${desconto2}`);
 }
-
